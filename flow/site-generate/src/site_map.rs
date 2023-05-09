4965f60834541fadc17159fe6b6d804d79986c20
namse <skatpgusskat@naver.com>
2023-05-09 08:38:14 +0000

    Add index.html to site_map

--- flow/site-generate/src/site_map.rs
+++ flow/site-generate/src/site_map.rs
@@ -4,17 +4,17 @@ pub fn save_site_map(md_files: &Vec<MdFile>) {
     let file_links = md_files
         .into_iter()
         .map(|md_file| {
-            format!(
-                "https://namseent.github.io/wiki/{}",
-                urlencoding::encode(
-                    md_file
-                        .contents_dir_relative_path
-                        .with_extension("html")
-                        .to_str()
-                        .unwrap()
-                )
+            urlencoding::encode(
+                md_file
+                    .contents_dir_relative_path
+                    .with_extension("html")
+                    .to_str()
+                    .unwrap(),
             )
+            .to_string()
         })
+        .chain(std::iter::once("index.html".to_string()))
+        .map(|x| format!("https://namseent.github.io/wiki/{x}"))
         .collect::<Vec<_>>()
         .join("\n");
 
